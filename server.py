d1047dcc21678718458293aaa3fe75e4b209fb52
Hackbright Student <info@hackbrightacademy.com>
2016-05-03 18:04:18 -0700

    end of tuesday

--- server.py
+++ server.py
@@ -25,6 +25,69 @@ def index():
 
     return render_template("homepage.html")
 
+@app.route("/users")
+def user_list():
+    """Show list of users."""
+
+    users = User.query.all()
+    return render_template("user_list.html", users=users)
+
+@app.route("/register")
+def register_form():
+    """Routes user to registration form"""
+
+
+    return render_template("registration_form.html")
+
+@app.route("/register", methods=['POST']):
+def handle_form_data():
+
+    password = request.form['password']
+    age = request.form['age']
+    email= request.form['email']
+    zipcode = request.form['zipcode']
+
+    # Check if the user_id is taken
+    if not is_user_in_db(user_id):
+        # Create a user
+        new_user = User(password=password, age=age, email=email, zipcode=zipcode)
+        db.session.add(new_user)
+        db.session.commit()
+
+    create_user_session(email, password, )
+    return redirect('/', 200)
+
+
+@app.route("/login" methods=['GET'])
+def user_login():
+
+    return render_template("login_form.html")
+
+@app.route("/login" methods=['POST'])
+def user_login():
+    email=request.form['email']
+    password=request.form['password']
+    # If the username exists
+    if is_user_in_db(email):
+        create_user_session(email, password)
+    # Else
+    else:
+        # Flash a message saying incorrect user_id or password
+
+    return redirect('/', 200)
+
+# Utility functions
+def is_user_in_db(email):
+    if User.query.filter_by(User.email == email).all():
+        return True
+    return False
+
+def create_user_session(email, password):
+    # password check
+    user_to_check = User.query.filter_by(User.email==email, User.password==password).one()
+    # flash message saying user logged in
+    flash('You were successfully logged in')
+
 
 if __name__ == "__main__":
     # We have to set debug=True here, since it has to be True at the point
